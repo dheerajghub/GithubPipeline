b5f1a78877892933adf68045c8cb0e02570af030
Dheeraj Kumar Sharma <dheerajkumarsharma050699@gmail.com>
2022-05-22 13:05:49 +0530

    Update import_provisioning.sh

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -14,10 +14,10 @@ cp provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 echo "List profiles"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
-security create-keychain -p "" build.keychain
-security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "123123" -A
+# security create-keychain -p "" build.keychain
+# security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "123123" -A
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+# security list-keychains -s ~/Library/Keychains/build.keychain
+# security default-keychain -s ~/Library/Keychains/build.keychain
+# security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+# security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
