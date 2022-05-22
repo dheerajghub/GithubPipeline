a4516daea3b64370b79178711a433024ef9e0fff
Dheeraj Kumar Sharma <dheerajkumarsharma050699@gmail.com>
2022-05-22 13:08:51 +0530

    Update import_provisioning.sh

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -5,14 +5,14 @@
 gpg --quiet --batch --yes --decrypt --passphrase="123123" --output provisioning/AppStoreCertificates.p12 provisioning/AppStoreCertificates.p12.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="123123" --output provisioning/demo.mobileprovision provisioning/demo.mobileprovision.gpg
 
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-echo "List profiles"
-ls ~/Library/MobileDevice/Provisioning\ Profiles/
-echo "Move profiles"
-cp provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-echo "List profiles"
-ls ~/Library/MobileDevice/Provisioning\ Profiles/
+# echo "List profiles"
+# ls ~/Library/MobileDevice/Provisioning\ Profiles/
+# echo "Move profiles"
+# cp provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+# echo "List profiles"
+# ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
 # security create-keychain -p "" build.keychain
 # security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "123123" -A
