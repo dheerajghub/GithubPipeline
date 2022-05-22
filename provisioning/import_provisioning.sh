4542145f32d0b5448bb691166e10e7dd2fd6f50b
Dheeraj Kumar Sharma <dheerajkumarsharma050699@gmail.com>
2022-05-22 12:28:54 +0530

    Update import_provisioning.sh

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -2,8 +2,8 @@
 
 # Decrypt the files
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output provisioning/AppStoreCertificates.p12 provisioning/AppStoreCertificates.p12.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output provisioning/demo.mobileprovision provisioning/demo.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="123123" --output provisioning/AppStoreCertificates.p12 provisioning/AppStoreCertificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="123123" --output provisioning/demo.mobileprovision provisioning/demo.mobileprovision.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
@@ -15,7 +15,7 @@ echo "List profiles"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
-security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
+security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "123123" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
